c5c7541942e7e4a11b413afc64280085b0187625
sirisha255 <112860662+sirisha255@users.noreply.github.com>
2023-03-29 11:23:30 +0200

    Add project files.

---
+++ WebApi_BT/Models/APIDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi_BT.Models
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions options) : base(options) 
+        { 
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ WebApi_BT/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi_BT.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Index { get; set; }
+        [Required]
+        [Column(TypeName ="nvarchar(250)'")]
+        public string? Product_Name { get; set; }
+
+        public string? Manufacturer { get; set; }
+        
+        public string? Description { get; set;}
+        
+        public string? BarCode { get; set; }
+
+        public string? Co2 { get; set; }
+
+        public string? Marks { get; set; }
+
+        public string? Origin { get; set;}
+    
+    
+    }
+
+}
